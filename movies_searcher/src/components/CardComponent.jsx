2894c23a57e483754fb495ec1c177f071c17ee9d
Santiago Junco <77699351+santi-junco@users.noreply.github.com>
2024-05-12 18:32:57 -0300

    Add more info button

--- movies_searcher/src/components/CardComponent.jsx
+++ movies_searcher/src/components/CardComponent.jsx
@@ -1,9 +1,17 @@
 export const CardComponent = ({ movie, url_img }) => {
+  const url_base = "https://www.themoviedb.org/movie/";
   return (
     <div key={movie.id} className="movie-card">
       <img src={`${url_img}${movie.poster_path}`} alt={movie.title} />
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
+      <a
+        className="btn btn-outline-info"
+        href={`${url_base}${movie.id}${movie.original_title}`}
+        target="blank"
+        role="button">
+        More info
+      </a>
     </div>
   );
 };
